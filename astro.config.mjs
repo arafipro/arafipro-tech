c1f48c6901954271540a31e4af16141b9e0fd3c3
Hiroki Chiyoda <contact@arafipro.com>
2024-08-19 20:59:33 +0900

    feat: 記事を追加
    フルスタックエンジニアになるために

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,15 +21,10 @@ export default defineConfig({
       },
       sidebar: [
         {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          label: "フルスタックエンジニアになるために",
+          autogenerate: {
+            directory: "fullstack",
+          },
         },
       ],
       customCss: ["./src/tailwind.css"],
