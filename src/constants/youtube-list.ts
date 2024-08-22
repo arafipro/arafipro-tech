5c617ab00349045eb770bb9f1200ea0bcfc5f178
Hiroki Chiyoda <contact@arafipro.com>
2024-08-22 15:56:27 +0900

    feat: YouTubeのリンクを追加

---
+++ src/constants/youtube-list.ts
@@ -0,0 +1,149 @@
+export const BackendList = [
+  {
+    date: "2024/08/20",
+    id: "ViLEETSKaAI",
+    title:
+      "【Cloudflare Workers】2024年最新！初心者でもわかるLangChain Expression Language (LCEL)の基礎",
+    pass: "/workers/langchain-openai-base-api/intro",
+  },
+  {
+    date: "2024/08/09",
+    id: "nVuZiBAXJo0",
+    title:
+      "【Cloudflare Workers】誰でも簡単に始められる！Bun+Hono+Drizzleで株式投資取引APIを作る",
+  },
+  {
+    date: "2024/08/02",
+    id: "gkylc_XAQJw",
+    title:
+      "【Cloudflare Pages】Neon DB+Drizzle ORMを使って、TODOアプリを作る（フロントエンド編）",
+  },
+  {
+    date: "2024/07/29",
+    id: "tFfUHpv2FUI",
+    title:
+      "【Cloudflare Pages】Neon DB+Drizzle ORMを使って、TODOアプリを作る（バックエンド編）",
+  },
+  {
+    date: "2024/07/18",
+    id: "rYEjzr0uTxY",
+    title: "【Formタグ】【React Hook Form】【zod】入力フォーム完全版",
+  },
+  {
+    date: "2024/07/12",
+    id: "6A2uFuJHZGE",
+    title:
+      "【React Native/Expo】データベースExpo SQLiteとExpo Routerを使って、スマートフォンのTODOアプリを作る",
+  },
+  {
+    date: "2024/07/04",
+    id: "oVF_WaLQnUw",
+    title:
+      "【Cloudflare Workers】Bun+HonoでREST APIを作成　drizzleを使ってデータベースのリレーションを実現！！",
+  },
+  {
+    date: "2024/06/25",
+    id: "ks2-LLyAD5Y",
+    title:
+      "【Cloudflare Workers】Bun+HonoでREST APIを作成　zodを使ってバリデーションチェック！！",
+  },
+  {
+    date: "2024/06/18",
+    id: "jaYkXW8II-k",
+    title:
+      "【Cloudflare Workers】Bun+HonoでREST APIを作成する方法！効果的なhonoルーティングの秘訣",
+  },
+  {
+    date: "2024/06/11",
+    id: "lmzd1eUpv2w",
+    title:
+      "【React Native/Expo51】最新Expo51！Expo RouterのTabs layoutを使ってみる",
+  },
+  {
+    date: "2024/06/04",
+    id: "5hAg40Y_VgE",
+    title:
+      "【React Hook Form】【zod】shadcn/uiを使って、リアルタイムエディタを作る",
+  },
+  {
+    date: "2024/05/28",
+    id: "yMnMdrgLIDw",
+    title:
+      "【react-markdown】ブログアプリで使える　Markdown-HTML 変換コンポーネントを作る",
+  },
+  {
+    date: "2024/05/20",
+    id: "f641dliqzwQ",
+    title:
+      "【Cloudflare Workers】Hono＋Cloudflare D1＋Prisma 20分でREST APIを作成",
+  },
+  {
+    date: "2024/05/13",
+    id: "EadIZK96lsE",
+    title: "【Auth.js】Webアプリ必須！NextAuth v5で認証機能付きヘッダーを作る",
+  },
+  {
+    date: "2024/05/06",
+    id: "PQeW8gLI5Eg",
+    title:
+      "【React Hook Form】【zod】shadcn/uiを使って、リッチな入力フォームを作る",
+  },
+  {
+    date: "2024/04/29",
+    id: "-3EoYGs4THw",
+    title:
+      "【Auth.js】NextAuth v5で認証機能を作って、Cloudflare Pagesにデプロイする",
+  },
+  {
+    date: "2024/04/22",
+    id: "RXN-JLfm77s",
+    title:
+      "【React Native/Expo50】REST APIとExpo Routerを使って、スマートフォンのTODOアプリを作る",
+  },
+  {
+    date: "2024/04/15",
+    id: "dAfk-eGIqAA",
+    title:
+      "【Cloudflare Pages】無料で公開！REST APIを使って、Next.jsでTODOアプリを30分で作る",
+  },
+  {
+    date: "2024/04/08",
+    id: "kCwHEXqoPsE",
+    title:
+      "【Cloudflare Workers】Hono＋Cloudflare D1＋Drizzle ORM 30分でREST APIを作成",
+  },
+  {
+    date: "2024/04/01",
+    id: "XyjACmtXqj0",
+    title:
+      "【Cloudflare Workers】無料で公開！ Hono＋Cloudflare D1でREST APIを作る",
+  },
+  {
+    date: "2024/02/26",
+    id: "6khQJ0zKb7Q",
+    title:
+      "【React Hook Form】超便利！React Hook Formで入力フォームをアップデート！！",
+  },
+  {
+    date: "2024/02/19",
+    id: "fPsz5OhpSDE",
+    title: "【Next.js14】【TypeScript】Webアプリ必須！入力フォームを作る",
+  },
+  {
+    date: "2023/12/20",
+    id: "86h8TOOJbkA",
+    title:
+      "【React】【TypeScript】Remotionで「Hello, World」が徐々に現れ、徐々に消える動画を作ってみた",
+  },
+  {
+    date: "2023/12/09",
+    id: "O6aVAIa9Mf8",
+    title:
+      "【React】【TypeScript】Remotionで「Hello, World」が飛び出す動画を作ってみた",
+  },
+  {
+    date: "2023/10/23",
+    id: "TNtTnt97Lds",
+    title: "【React】【TypeScript】Remotionで動画作成を試してみた",
+  },
+];
